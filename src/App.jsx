0729fc80605ae449ef704cf1aa487823811f43ba
Agostina <lozagostina@gmail.com>
2022-09-25 20:05:03 -0300

    agrego react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
-  <h1>prueba</h1>
+  <h1 className='display-4'>prueba</h1>
     </div>
   );
 }
