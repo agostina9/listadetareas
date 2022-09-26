dc1ba5044d66729184c910a21fdb8dd1e1b33419
Agostina <lozagostina@gmail.com>
2022-09-25 23:49:11 -0300

    agrego formularios items y li

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
-  <h1 className='display-4'>prueba</h1>
-    </div>
+
+    <Container className='my-5'>
+      <h1 className='display-4 text-center'>Lista de tareas</h1>
+<hr />
+<Formulario></Formulario>
+    </Container>
+
   );
 }
 
---
+++ src/components/ListaTarea.jsx
@@ -0,0 +1,12 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import ItemTarea from './ItemTarea';
+
+const ListaTarea = () => {
+    return (
+        <ListGroup>
+            <ItemTarea></ItemTarea>
+       </ListGroup>
+    );
+};
+
+export default ListaTarea;
